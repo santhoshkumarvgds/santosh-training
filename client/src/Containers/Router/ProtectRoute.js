e4f4961185076ba8fde57682fce219ae826a08a9
santhoshvgds <65397534+santhoshvgds@users.noreply.github.com>
2020-06-11 21:15:49 +0530

    Update ProtectRoute.js

--- client/src/Containers/Router/ProtectRoute.js
+++ client/src/Containers/Router/ProtectRoute.js
@@ -1,47 +1,90 @@
-import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
-import { UserContext } from "../Context/Context";
-import Auth from "../Auth/Auth";
+import history from "../../history";
 
-export default function ProtectRoute({ component: Component, roles, ...rest }) {
-  const { role, loginStatus } = useContext(UserContext);
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        // alert(
-        //   role + " " + roles + " " + loginStatus + " " + Auth.authenticateStatus
-        // );
-        if (Auth.authenticateStatus == roles) {
-          return <Component {...props} />;
-        }
-        if (
-          Auth.authenticateStatus == undefined &&
-          role == undefined &&
-          !loginStatus
-        ) {
-          return (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: { from: props.location },
-              }}
-            />
-          );
-        } else if (role) {
-          return (
-            <Redirect
-              to={{
-                pathname: Auth.authenticateStatus,
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
-        }
-        return <Component {...props} />;
-      }}
-    />
-  );
+class Auth {
+  constructor(props) {
+    this.authenticateStatus = false;
+    this.pendingStatus = false;
+    this.userEmail = "";
+    this.userPassword = "";
+  }
+
+  async login(cb) {
+    // localStorage.clear();
+    const [email, password] = [this.userEmail, this.userPassword];
+    const response = await fetch("http://localhost:4000/user/login", {
+      method: "post",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    const body = await response.json();
+    if (body.message === "success" && body.role === "User") {
+      // localStorage.setItem("token", body.token);
+      this.authenticateStatus = "user";
+      //   history.push("/user");
+      cb();
+    } else if (body.message === "success" && body.role === "Admin") {
+      // localStorage.setItem("token", body.token);
+      this.authenticateStatus = "admin";
+      //   history.push("/admin");
+      cb();
+    } else if (body.message === "success" && body.role === "Seller") {
+      // localStorage.setItem("token", body.token);
+      this.authenticateStatus = "seller";
+      //   history.push("/seller");
+      cb();
+    } else if (
+      body.message === "Approvel pending" &&
+      body.status !== "reject"
+    ) {
+      // localStorage.setItem("redirect", "pendingapprovel");
+      this.authenticateStatus = "pendingapprovel";
+      //   history.push("/pendingapprovel")
+      cb();
+    } else if (
+      body.message === "Approvel pending" &&
+      body.status === "reject"
+    ) {
+      alert("You're rejected by Admin!!!");
+      this.authenticateStatus = "reject";
+    } else {
+      alert(body.message);
+    }
+  }
+
+  async logout() {
+    // this.authenticateStatus = false;
+    const response = await fetch("http://localhost:4000/user/logout", {
+      method: "post",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+    const body = await response.json();
+    if (body.status) {
+      history.push("/");
+    } else {
+      alert("logout failed");
+    }
+  }
+
+  async authuser() {
+    const response = await fetch("http://localhost:4000/user/getinfo", {
+      method: "post",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await response.json();
+    this.authenticateStatus = body.role.toLowerCase();
+    return body;
+  }
+  isAuthenticate() {
+    return this.authenticateStatus;
+  }
 }
+
+export default new Auth();
